2abba205d329805136413d80db89479ada45af0d
Malohu <malohu@gmail.com>
2014-06-06 17:39:32 +0200

    ça avance un peu

--- Ad-hoc Network/src/lsa/LSAMessage.java
+++ Ad-hoc Network/src/lsa/LSAMessage.java
@@ -1,11 +1,45 @@
 package lsa;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+
 
 public class LSAMessage
 {
+	private LinkedList<InetAddress> neighboursAdresses;
+	short sequenceNumber;
+	short numberofNeighbours;
 	
-	LSAMessage(StringBuffer message)
+	LSAMessage(ByteBuffer message)
 	{
+		char type = message.getChar();
 		
+		if (type != 'l') 
+		
+		message.getChar();
+		short messageSize = message.getShort();
+		sequenceNumber = message.getShort();
+		numberofNeighbours = message.getShort();
+		
+		
+		byte[] addresse = new byte[4];
+		for (int i = 0; i < numberofNeighbours ; i++) {
+			
+			for (int j = 0; j < 4; j++) {
+				addresse[j]=message.get();
+			}
+			try {
+				neighboursAdresses.add(InetAddress.getByAddress(addresse));
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+	
+	ByteBuffer toBuffer() {
+		ByteBuffer buffer = ByteBuffer.allocate(1000);
+	}
+	
 }
