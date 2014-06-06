36ac5512fd1d874c5a3dc6760a3a39aac3141afd
Gurvan <gurvan.lhostis@polytechnique.edu>
2014-06-06 17:39:18 +0200

    rien

--- Ad-hoc Network/src/Main.java
+++ Ad-hoc Network/src/Main.java
@@ -1,9 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world bitches!");
+		InetAddress address = null;
+		try {
+			 address = InetAddress.getByName("127.0.0.1");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(address.toString());
 	}
 
 }
--- Ad-hoc Network/src/hello/HelloMessage.java
+++ Ad-hoc Network/src/hello/HelloMessage.java
@@ -1,11 +1,70 @@
 package hello;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+
 public class HelloMessage
 {
-	private HelloTable neighbours;
+	private LinkedList<InetAddress> heardNeighbours;
+	private LinkedList<InetAddress> symmetricNeighbours;
 	
-	HelloMessage(StringBuffer message)
+	HelloMessage(ByteBuffer message)
 	{
+		byte[] byteAddress = new byte[4];
+		if (message.getChar() == 'h') {
+			message.get();
+			message.get();
+			message.get();
+			//Heard neighbours listing
+			int numberOfHeard = message.getShort();
+			message.get();
+			message.get();
+			message.get();
+			heardNeighbours = new LinkedList<InetAddress>();
+			for (int i = 0; i<numberOfHeard; i++) {
+				for (int j = 0; j<4; j++) {
+					byteAddress[j] = message.get();
+				}
+				try {
+					heardNeighbours.add(
+							InetAddress.getByAddress(byteAddress));
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				}
+			}
+			//Symmetric neighbours listing
+			int numberOfSymmetric = message.getShort();
+			message.get();
+			message.get();
+			message.get();
+			heardNeighbours = new LinkedList<InetAddress>();
+			for (int i = 0; i<numberOfHeard; i++) {
+				for (int j = 0; j<4; j++) {
+					byteAddress[j] = message.get();
+				}
+				try {
+					symmetricNeighbours.add(
+							InetAddress.getByAddress(byteAddress));
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public ByteBuffer toBuffer() 
+	{
+		int bufferSize = 32*(3 
+				+ heardNeighbours.size()
+				+ symmetricNeighbours.size());
+		ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+		buffer.putChar('h');
+		buffer.put((byte) 0);
+		buffer.putShort(value)
+
 		
 	}
+
 }
