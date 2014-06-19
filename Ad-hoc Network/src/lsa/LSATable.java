4c6cce69adc881e1bc1c0438df39f584af7f0d21
Gurvan <gurvan.lhostis@polytechnique.edu>
2014-06-19 18:33:18 +0200

    lsa compare debug

--- Ad-hoc Network/src/lsa/LSATable.java
+++ Ad-hoc Network/src/lsa/LSATable.java
@@ -36,8 +36,9 @@ public class LSATable{
 	
 	public boolean isLatest(InetAddress address, ByteBuffer buffer)
 	{
-		return table.get(address).sequenceNumber() 
-				== 256*buffer.array()[8]+buffer.array()[9];
+		LSAMessage latest = table.get(address);
+		return latest == null || latest.sequenceNumber() 
+				< 256*buffer.array()[8]+buffer.array()[9];
 	}
 	
 	public int numberOfNodes()
