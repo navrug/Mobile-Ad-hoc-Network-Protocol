6551257e652a724bd12e79641af9b03e0383b858
Gurvan <gurvan.lhostis@polytechnique.edu>
2014-06-27 14:08:45 +0200

    Table locks, LSATable equals correction

--- Ad-hoc Network/src/hello/HelloTable.java
+++ Ad-hoc Network/src/hello/HelloTable.java
@@ -3,27 +3,41 @@ package hello;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Hashtable;
+import java.util.concurrent.locks.ReentrantLock;
 
 import utilities.IP;
 import lsa.LSAMessage;
 
+/*
+ * This class has to be concurrency safe because MessageThread
+ * adds messages to the table while PacketManager uses the
+ * table to create and sends new messages.
+ */
 public class HelloTable
 {
 	private final Hashtable<IP, HelloMessage> table;
 	private static short sequenceNumber = 0;
+	private ReentrantLock lock = new ReentrantLock();
 
 	public HelloTable()
 	{
 		table = new Hashtable<IP, HelloMessage>();
 	}
-	
+
 	public void addHello(IP neighbor, HelloMessage message)
 	{
-		table.put(neighbor, message);
+		try {
+			lock.lock();
+			table.put(neighbor, message);
+		}
+		finally {
+			lock.unlock();
+		}
 	}
-	
+
 	public HelloMessage createHello()
 	{
+
 		IP myAddress = null;
 		try {
 			myAddress = new IP(InetAddress.getLocalHost());
@@ -31,17 +45,21 @@ public class HelloTable
 			e.printStackTrace();
 		}
 		HelloMessage result = new HelloMessage(myAddress);
-		for (IP neighbor : table.keySet())
-		{
-			if (table.get(neighbor).isSymmetric(myAddress))
-				result.addSymmetric(neighbor);
-			else
-				result.addHeard(neighbor);
-
+		try {
+			lock.lock();
+			for (IP neighbor : table.keySet()) {
+				if (table.get(neighbor).isSymmetric(myAddress))
+					result.addSymmetric(neighbor);
+				else
+					result.addHeard(neighbor);
+			}
+		}
+		finally {
+			lock.unlock();
 		}
 		return result;
 	}
-	
+
 	public LSAMessage createLSA()
 	{
 		IP myAddress = null;
@@ -52,10 +70,16 @@ public class HelloTable
 		}
 		LSAMessage result = 
 				new LSAMessage(myAddress, sequenceNumber++);
-		for (IP neighbor : table.keySet())
-		{
-			if (table.get(neighbor).isSymmetric(myAddress))
-				result.addNeighbor(neighbor);
+		try {
+			lock.lock();
+			for (IP neighbor : table.keySet())
+			{
+				if (table.get(neighbor).isSymmetric(myAddress))
+					result.addNeighbor(neighbor);
+			}
+		}
+		finally {
+			lock.unlock();
 		}
 		return result;
 	}
--- Ad-hoc Network/src/lsa/LSATable.java
+++ Ad-hoc Network/src/lsa/LSATable.java
@@ -14,12 +14,13 @@ import utilities.IP;
 public class LSATable{
 	private final Hashtable<IP, LSAMessage> table = new Hashtable<IP, LSAMessage>();
 	private final RoutingTable routingTable;
+	private ReentrantLock lock = new ReentrantLock(); 
 
-	public LSATable(ReentrantLock lock)
+	public LSATable(ReentrantLock netLock)
 	{
-		routingTable = new RoutingTable(lock);
+		routingTable = new RoutingTable(netLock);
 	}
-	
+
 	/*
 	 *  Function called when a LSA is received.
 	 *  If the message has a newer sequence number, we put it in the table,
@@ -27,22 +28,35 @@ public class LSATable{
 	 */
 	public void addLSA(IP neighbor, LSAMessage message)
 	{
-		LSAMessage oldMessage = table.get(neighbor);
-		if (oldMessage == null || 
-				oldMessage.sequenceNumber() < message.sequenceNumber()) {
-			table.put(neighbor, message);
-			if (!oldMessage.equals(message)) {
-				routingTable.updateGraph(this);
-				routingTable.writeTable();
+		try {
+			lock.lock();
+			LSAMessage oldMessage = table.get(neighbor);
+			if (oldMessage == null || 
+					oldMessage.sequenceNumber() < message.sequenceNumber()) {
+				table.put(neighbor, message);
+				if (!message.equals(oldMessage)) {
+					routingTable.updateGraph(this);
+					routingTable.writeTable();
+				}
 			}
 		}
+		finally {
+			lock.unlock();
+		}
 	}
-	
+
 	//Takes in  consulting mode, returns the same mode
 	public boolean isLatest(IP address, ByteBuffer buffer)
 	{
 		buffer.getDouble(); //Move of 8 bytes
-		LSAMessage latestInTable = table.get(address);
+		LSAMessage latestInTable = null;
+		try {
+			lock.lock();
+			latestInTable = table.get(address);
+		}
+		finally {
+			lock.unlock();
+		}
 		if (latestInTable!=null)
 			System.out.println("isLatest : "+latestInTable.sequenceNumber());
 		System.out.println(latestInTable == null);
@@ -50,25 +64,50 @@ public class LSATable{
 		buffer.position(0);
 		return latestInTable == null || latestInTable.sequenceNumber() 
 				< sequence;
+
 	}
 
 	public int numberOfNodes()
 	{
-		return table.size();
+		try {
+			lock.lock();
+			return table.size();
+		}
+		finally {
+			lock.unlock();
+		}
 	}
 
 	public Set<IP> addresses()
 	{
-		return table.keySet();
+		try {
+			lock.lock();
+			return table.keySet();
+		}
+		finally {
+			lock.unlock();
+		}
 	}
 
 	public Collection<LSAMessage> messages()
 	{
-		return table.values();
+		try {
+			lock.lock();
+			return table.values();
+		}
+		finally {
+			lock.unlock();
+		}
 	}
 
 	public boolean isConnectedTo(IP a, IP b)
 	{
-		return (table.get(a)!=null)&&table.get(a).neighbors().contains(b);
+		try {
+			lock.lock();
+			return (table.get(a)!=null)&&table.get(a).neighbors().contains(b);
+		}
+		finally {
+			lock.unlock();
+		}
 	}
 }
