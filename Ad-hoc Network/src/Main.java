fd92b04072bb2db1ec07028959d0483d5ef11148
Malohu <malohu@gmail.com>
2014-06-27 14:04:28 +0200

    Main

--- Ad-hoc Network/src/Main.java
+++ Ad-hoc Network/src/Main.java
@@ -19,23 +19,7 @@ import lsa.LSAMessage;
 public class Main {
 
 	public static void main(String[] args) throws SocketException, UnknownHostException {
-
-		NetworkGraph g = new NetworkGraph();
-		IP own = new IP(InetAddress.getLocalHost());
-		IP a = new IP(1,1,1,1);
-		IP b = new IP(1,1,1,2);
-		IP c = new IP(1,1,1,3);
-		IP d = new IP(1,1,1,4);
-		g.addEdge(a,own);
-		g.addEdge(own,a);
-		//System.out.println(g.neighbors(own).toArray()[0]);
-		g.addEdge(own,b);
-		g.addEdge(b,own);
-		g.addEdge(own,c);
-		g.addEdge(c,own);
-		g.addEdge(c,d);
-		g.addEdge(d,c);
-		Printer p = new Printer(g, true);
-		p.refresh();
+		PacketManager packet = new PacketManager();
+		packet.run();
 	}
 }
